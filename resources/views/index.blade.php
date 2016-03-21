f0d1e18aaa2be9c46263b2b23b4a8ab481659d74
ramramtul <ramramtul@users.noreply.github.com>
2016-03-21 11:40:07 +0700

    Folder untuk develop

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	    <title>Laravel</title>
+
+	    <!-- Fonts -->
+	    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+	    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+
+	    <!-- Styles -->
+	    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+	    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+	    <style>
+	        body {
+	            font-family: 'Lato';
+	        }
+
+	        .fa-btn {
+	            margin-right: 6px;
+	        }
+	    </style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="form-row">
+				<div class="panel-def">
+					<div class="panel-reg">Register</div>
+					<div class="form-panel">
+						<form></form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
