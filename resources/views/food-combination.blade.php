07adab956cb250a4c073154cbaed1e2cfc5c755b
Putra Muttaqin <putramuttaqin@gmail.com>
2016-04-11 09:16:45 +0700

    nambahin layout di food-combination.php

--- resources/views/food-combination.blade.php
+++ resources/views/food-combination.blade.php
@@ -1,5 +1,13 @@
+@extends('layout')
+
+@section('title')
+	<title>MargoFoodies - Hasil Pencarian Makanan</title>
+@stop
+
+@section('content')
 <div class="container">
 	@foreach($data as $menu)
 		<p>{{$menu->nama}} harga {{$menu->harga}}</p>
 	@endforeach
 </div>
+@stop
