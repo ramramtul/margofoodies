a21430f9577648864b2bcdcc0e740b1317a855af
SeptivianaSavitri <septivianasavitri37@gmail.com>
2016-04-27 21:09:16 +0700

    ada beberapa tambahan select pada edit menu

--- app/Http/Controllers/menusController.php
+++ app/Http/Controllers/menusController.php
@@ -39,10 +39,13 @@ class menusController extends CrudController{
 			
 			$this->addStylesToGrid();
 
-			$this->grid->paginate(10);
+			$this->grid->paginate(20);
 
 			//$this->grid->add('name', 'Name', true); // allow ordering by this column
 			$this->grid->orderBy('id','asc'); //default orderby
+			$this->grid->add('nama', 'Nama', true);
+			$this->grid->add('id', 'Id', true);
+			$this->grid->add('harga', 'Harga', true);
 
                  
         return $this->returnView();
@@ -55,21 +58,22 @@ class menusController extends CrudController{
         	$this->edit = \DataEdit::source(new \App\Menus());
 
 			$this->edit->label('Edit Menu');
+			$this->edit->add('id', '', 'auto')->insertValue('1080000');
 
-			$this->edit->add('id', 'id', 'text');
+			//$this->edit->add('id', 'id', 'text');
 			$this->edit->add('nama', 'Nama', 'text');
 			$this->edit->add('harga', 'Harga', 'text');
-			$this->edit->add('id_restoran', 'Id_Restoran', 'text');
+			$this->edit->add('id_restoran', 'Id_Restoran', 'select')->options(\App\Restorans::lists("nama", "id")->all());
 			$this->edit->add('kapasitas', 'Kapasitas', 'text');
-			$this->edit->add('jenis', 'Jenis', 'text');
-			$this->edit->add('kategori', 'Kategori', 'text');
+			$this->edit->add('jenis', 'Jenis', 'select')->option('Makanan utama','Makanan utama')->option('Makanan penutup','Makanan penutup')->option('Cemilan','Cemilan')->option('Minuman','Minuman')->option('Other','Other');
+			$this->edit->add('kategori', 'Kategori', 'select')->option('Indonesian','Indonesian')->option('Other','Other')->option('Asian','Asian')->option('Coffee','Coffee')->option('Western','Western')->option('Bakery','Bakery')->option('Japanese','Japanese');
 
 			$this->edit->add('deskripsi', 'Deskripsi', 'text');
 			$this->edit->add('id_photo', 'Id_Photo', 'text');
 			$this->edit->add('rate', 'Rate', 'text');
 			$this->edit->add('jumlah_tested', 'Jumlah_Tested', 'text');
-			$this->edit->add('Is_Paket_Tanpa_Minum', 'is_Paket_Tanpa_Minum', 'text');
-			$this->edit->add('Is_Paket_Dgn_Minum', 'is_Paket_Dgn_Minum', 'text');
+			$this->edit->add('Is_Paket_Tanpa_Minum', 'is_Paket_Tanpa_Minum', 'radiogroup')->option('0', 'Tidak')->option('1', 'Ya');
+			$this->edit->add('Is_Paket_Dgn_Minum', 'is_Paket_Dgn_Minum', 'radiogroup')->option('0', 'Tidak')->option('1', 'Ya');
        
         return $this->returnEditView();
     }    
